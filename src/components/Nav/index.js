f4d62ccc4ab27d5bde65390ff7cbdcb3dedaccd8
Brian Halpin <brian.t.halpin@gmail.com>
2022-07-21 13:52:37 -0400

    Begin styling nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,26 @@
 import './nav.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { ARTICLES } from '../../utils/actions';
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 function Nav() {
     const categories = ['Business','Entertainment','Environment','Food','Health','Politics','Science','Sports','Technology','Top','World']
-    
+    const [open,setOpen] = useState(false);
+
+    const close = ({target}) => {
+        console.log(target)
+        open?setOpen(false):setOpen(true);
+    }
     return (
-        <div className="nav">
+        <div className="nav" onClick={e=>close(e)}>
+            <div className={open?'expand':'collapse'} >
             {categories.map((category,index)=>{
                 return (
-                    <div className="navLink" key={index}>{category}</div>
+                    <div className={`navLink`} key={index}>{category}</div>
                 )
             })}
+
+            </div>
         </div>
     );
 }
