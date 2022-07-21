77a783be381c9adea1aa284db3d27a9e3609b96f
Brian Halpin <brian.t.halpin@gmail.com>
2022-07-21 00:04:10 -0400

    Add merge sort algorith to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,81 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import {Card} from '../../components';
 import './home.css';
 import {results} from '../../tempData';
 
 function Home() {
-    const [search,setSearch] = useState('')
+    const [search,setSearch] = useState('');
+    const [articles,setArticles] = useState([])
+    const [order,setOrder] = useState('asc');
+    const changeOrder = () => {
+        if (order === 'asc'){
+            setOrder('desc');
+        } else {
+            setOrder('asc');
+        }
+    }
+    const mergeResults = (left,right) => {
+        let merged = [];
+        let i = 0;
+        let j = 0;
+        while (i<left.length && j<right.length){
+            if (order==='asc'){
+                if (left[i].title <= right[j].title){
+                    merged.push(left[i]);
+                    i++;
+                } else {
+                    merged.push(right[j]);
+                    j++;
+                }
+                
+            } else {
+                if (left[i].title >= right[j].title){
+                    merged.push(left[i]);
+                    i++;
+                } else {
+                    merged.push(right[j]);
+                    j++;
+                }
+
+            }
+        }
+
+        while (i<left.length){
+            merged.push(left[i]);
+            i++;
+        }
+        while (j<right.length){
+            merged.push(right[j]);
+            j++;
+        }
+
+        return merged;
+
+    }
+    const sortResults = (arr) => {
+        if (arr.length<=1){
+            return arr;
+        }
+        const middle = Math.floor(arr.length/2);
+        const left = sortResults(arr.slice(0,middle));
+        const right = sortResults(arr.slice(middle));
+
+        return mergeResults(left,right)
+    }
+    useEffect(()=>{
+        setArticles(sortResults(results));
+    },[order])
+
     return (
         <div className="home">
             <div>
                 <input type="text" placeholder="search" value={search} onChange={(e)=>setSearch(e.target.value)}></input>
                 <button onClick={()=>setSearch('')}>Clear</button>
             </div>
-
+            <button onClick={changeOrder}>change</button>
             <div>
-                {results.filter(article=>article.title.toLowerCase().includes(search.toLowerCase())).map((article,index)=><Link to={`/article/${index}`} key={index}><Card article={article} /></Link>)}
+                {articles.filter(article=>article.title.toLowerCase().includes(search.toLowerCase())).map((article,index)=><Link to={`/article/${index}`} key={index}><Card article={article} /></Link>)}
             </div>
         </div>
     );
