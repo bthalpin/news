66710482e83a3066773566d31be9d73e5f913992
Brian Halpin <brian.t.halpin@gmail.com>
2022-07-22 02:10:46 -0400

    Add back button on navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,9 +2,11 @@ import './nav.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { ARTICLES,CHANGE_TOPIC } from '../../utils/actions';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
     const [state,dispatch] = useStoreContext();
+    const navigate = useNavigate()
     const categories = ['General','Business','Entertainment','Health','Science','Sports','Technology']
     const [open,setOpen] = useState(true);
 
@@ -18,11 +20,15 @@ function Nav() {
         e.stopPropagation()
         !open&&(e.target.className==='menu expand'||e.target.className==='menuImage')?setOpen(true):setOpen(false);
     }
+    console.log(window.location.pathname)
     return (
         <div className="nav">
 
             <div className={`navLinkContainer ${open?'expanded':''}`}  onClick={e=>close(e)} >
-                <div className={`menu ${open?'collapse':'expand'}`}  onClick={e=>close(e)} ><img className="menuImage" src="menu.png" alt="menu"></img></div>
+                {window.location.pathname==='/'?<></>:
+                <div className={`${open?'collapse':''}`} onClick={()=>navigate(-1)}>Back</div>
+                 }
+                <div className={`menu ${open?'collapse':'expand'}`}  onClick={e=>close(e)} ><img className="menuImage" src="./menu.png" alt="menu"></img></div>
                 <div className={open?'expand':'collapse'} >
                 {categories.map((category,index)=>{
                     return (
