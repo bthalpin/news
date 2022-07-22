41b7bbd89e0b0243b60d4eecccae645d64bcebbf
Brian Halpin <brian.t.halpin@gmail.com>
2022-07-21 23:12:24 -0400

    Add responsiveness to navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,21 +4,23 @@ import { ARTICLES } from '../../utils/actions';
 import { useState } from 'react';
 
 function Nav() {
-    const categories = ['business','entertainment','general','health','science','sports','technology']
-    const [open,setOpen] = useState(false);
+    const categories = ['Business','Entertainment','General','Health','Science','Sports','Technology']
+    const [open,setOpen] = useState(true);
 
-    const close = ({target}) => {
-        console.log(target)
+    const close = (e) => {
+        e.stopPropagation()
         open?setOpen(false):setOpen(true);
+        // setOpen(closing)
     }
     return (
-        <div className="nav" onClick={e=>close(e)}>
-            <div className={open?'expanded':''}>
-                <div className={`menu ${open?'collapse':'expand'}`} >OPEN</div>
+        <div className="nav">
+
+            <div className={`navLinkContainer ${open?'expanded':''}`}  onClick={e=>close(e)} >
+                <div className={`menu ${open?'collapse':'expand'}`}  onClick={e=>close(e)} >OPEN</div>
                 <div className={open?'expand':'collapse'} >
                 {categories.map((category,index)=>{
                     return (
-                        <div className={`navLink`} key={index}>{category}</div>
+                        <div className={`navLink`} key={index} onClick={()=>console.log(category)}>{category}</div>
                     )
                 })}
 
