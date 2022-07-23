c43f3760f458715876bcdfdfaf6d4c85a202b409
Brian Halpin <brian.t.halpin@gmail.com>
2022-07-23 15:19:52 -0400

    Add hover effect for buttons

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
 
         <StoreProvider>
-          <ScrollToTop />
+          {/* <ScrollToTop /> */}
           <Nav />
             <Routes>
               <Route
